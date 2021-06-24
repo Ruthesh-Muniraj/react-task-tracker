e2e80de5c0318e64305fb28303c587d0024439ec
Ruthesh-Muniraj <rutheshmuniraj@outlook.com>
2021-06-24 14:20:39 +0530

    added json-server, routes.-- Project Complete 🎉

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import { useLocation } from 'react-router-dom'
+
+const Footer = () => {
+    const location = useLocation()
+    return (
+        <footer>
+            {
+                location.pathname === '/' && <>
+                    <p>Copyright &copy; 2021</p>
+                    <Link to='/about'>About</Link>
+                </>
+            }
+        </footer>
+    )
+}
+export default Footer
